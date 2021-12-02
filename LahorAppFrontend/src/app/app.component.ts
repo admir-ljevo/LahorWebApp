a3c786a63e71115b0c257d21e2c993dc7d3f8582
Amir Karaga <83365970+amir-karaga@users.noreply.github.com>
2021-12-02 23:05:03 +0100

    Generisani osnovni controlleri AddGet

--- LahorAppFrontend/src/app/app.component.ts
+++ LahorAppFrontend/src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'LahorAppFrontend';
+export class AppComponent{
+
+  bracniStatusiPodaci:any;
+  constructor(private httpKlijent: HttpClient) {
+  }
+
+
+getBracniStatus(){
+    return this.bracniStatusiPodaci;
+}
+  testirajWebApi() :void
+  {
+    this.httpKlijent.get("https://localhost:44365/BracniStatusi/GetAll").subscribe(x=>{
+      this.bracniStatusiPodaci = x;
+    });
+  }
 }
