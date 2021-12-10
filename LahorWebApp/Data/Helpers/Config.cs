9ffc9dd5a8aa7ada0aa94336443e35e522be20de
Amir Karaga <83365970+amir-karaga@users.noreply.github.com>
2021-12-10 15:54:41 +0100

    Dodane osnovne helper metode za pagedList i Config za pohranu putanje do slike

---
+++ LahorWebApp/Data/Helpers/Config.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Helpers
+{
+    public class Config
+    {
+        public static string AplikacijURL = "https://localhost:44339/";
+
+        public static string Slike => "profile_images/";
+        public static string SlikeURL => AplikacijURL + Slike;
+        public static string SlikeFolder => "wwwroot/" + Slike;
+    }
+}
